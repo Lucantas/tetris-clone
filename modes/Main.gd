428062644d39cad30b5830715f186ce14534a24f
Lucas <lucas.lucantas38@gmail.com>
2019-06-18 19:38:15 -0300

    creating the logic behind movement and game rules

---
+++ modes/Main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const GRID_SIZE = Vector2(16,16)
+
+var row : int = 32
+var column : int = 16
+
+var game_speed : float = 1
+
+func _ready():
+	# TODO: Create a Timer to handle the piece movement speed
+	# TODO: Start the timer
+	pass
+
+func _process(delta):
+	# 
+	pass
---
+++ player/Player.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+# TODO: create a variable with the root node of the game mode scene
+
+# TODO: create an array with all the pieces
+
+func _ready():
+	# TODO: randomize to retrieve one item of the array
+	# TODO: give the piece to the player
+	
+	# TODO: randomize again to get the next piece, to be displayed at the user
+	pass
+
+func _input(event):
+	if event is InputEventKey:
+		# TODO: listen to the inpts to move and rotate the piece
+		pass
+		
